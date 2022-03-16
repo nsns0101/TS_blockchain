78ca9fd3e69084041890c604dbf4d25672e77644
nsns0101 <nsns0101@naver.com>
2022-03-17 08:26:44 +0900

    Interfaces on TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
-const name1 = "Lee Jae Young",
-    age = 24,
-    gender = "male"
-
-//매개변수에 타입을 지정
-// ?는 선택 매개변수
-// string타입으로 반환
-const sayHi = ( name1 : string, age : number, gender? : string): string => {
-    return `Hello ${name1}, you are ${age}, you are a ${gender}`;
+interface Human {
+    name : string;
+    age : number;
+    gender : string
 }
 
-console.log(sayHi(name1, age, gender));
+const person = {
+    name : "JaeYoung",
+    age : 26,
+    gender : "male"
+}
+
+//
+const sayHi = (person : Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+}
+
+console.log(sayHi(person));
 
 export {};
