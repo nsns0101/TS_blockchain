aaf2444f9c15ff75483b3bd3a700b8507dc16a57
nsns0101 <nsns0101@naver.com>
2022-03-17 07:56:11 +0900

    first typescript

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("hello");
+const name1 = "Lee Jae Young",
+    age = 24,
+    gender = "male"
+
+//매개변수에 타입을 지정
+// ?는 선택 매개변수
+// string타입으로 반환
+const sayHi = ( name1 : string, age : number, gender? : string): string => {
+    return `Hello ${name1}, you are ${age}, you are a ${gender}`;
+}
+
+console.log(sayHi(name1, age, gender));
+
+export {};
