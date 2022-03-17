03308db9e15399e2b5cea6da1cf05f8c06db847c
nsns0101 <nsns0101@naver.com>
2022-03-18 02:00:06 +0900

    Blockchain Creating a Bloack part Two, import

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+//기본적인 typescript import 방법
+import * as CrtptoJS from "crypto-js";
+
 class Block {
     public index: number;
     public hash: string;
@@ -5,6 +8,14 @@ class Block {
     public data: string;
     public timestamp: number;
     
+    //생성하지 않고도 불러올 수 있게 static로 선언
+    static calculateBlockHash = (
+        index: number, 
+        previousHash: string, 
+        timestamp: number, 
+        data: string
+        ): string => CrtptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
         index: number,
         hash: string,
@@ -20,13 +31,23 @@ class Block {
     }
 }
 
+//블록 생성하지 않고 부르기
+// Block.calculateBlockHash();
+
+//블록 생성
 const genesisBlock : Block = new Block(0, "20202020202020", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 //console.log(blockchain);
 
 //블록이 아니라 push가 작동이 안됨
 //blockchain.push("stuff");
+
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};
 
